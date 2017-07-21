27ac025a939ba4c0dced39a3fcd7edac912d3cad
zc7025 <1531952273@qq.com>
2017-07-21 19:45:57 +0800

    实现ServiceServlet和更改左部侧边栏链接

--- src/main/java/com/gs/controller/ServiceServlet.java
+++ src/main/java/com/gs/controller/ServiceServlet.java
@@ -21,13 +21,157 @@ public class ServiceServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String method = WebUtil.getReqMethod(req);
-        if (method.equals("comreg")) {
+        if (method.equals("comreg")) { // 公司注册
             showComregPage(req, resp);
+        } else if(method.equals("sort_out")) {  // 乱账整理
+            showSortOutPage(req,resp);
+        } else if(method.equals("bookkeep")) { // 代理记账
+            showBookkepingPage(req,resp);
+        } else if(method.equals("com_change")) { // 企业变更
+            showComChangePage(req,resp);
+        } else if(method.equals("com_cancal")) { // 公司注销
+            showComCancalPage(req,resp);
+        } else if(method.equals("license")) { // 食品流通许可证
+            showLicensePage(req,resp);
+        } else if(method.equals("service_permit")) { // 餐饮服务许可证
+            showServicePermitPage(req,resp);
+        } else if(method.equals("recognition")) { // 进口权认定
+            showRecognitionPage(req,resp);
+        } else if(method.equals("identified")) { // 一般纳税人认定
+            showIdentifiedPage(req,resp);
+        } else if(method.equals("trademark_reg")) { // 商标注册
+            showTrademarkRegPage(req,resp);
         }
     }
 
-    private void showComregPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute(Constants.CURRENT_PAGE, "service");
+    /**
+     * 乱账整理
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showSortOutPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException  {
+        req.setAttribute(Constants.CURRENT_PAGE, "sort_out");
+        req.getRequestDispatcher("/WEB-INF/views/service/account_sort_out.jsp").forward(req, resp);
+    }
+
+    /**
+     * 公司注册
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showComregPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute(Constants.CURRENT_PAGE, "comreg");
         req.getRequestDispatcher("/WEB-INF/views/service/comreg.jsp").forward(req, resp);
     }
+
+    /**
+     * 代理记账
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showBookkepingPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException  {
+        req.setAttribute(Constants.CURRENT_PAGE, "bookkeep");
+        req.getRequestDispatcher("/WEB-INF/views/service/bookkeeping.jsp").forward(req, resp);
+    }
+
+    /**
+     * 企业变更
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showComChangePage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute(Constants.CURRENT_PAGE, "com_change");
+        req.getRequestDispatcher("/WEB-INF/views/service/business_change.jsp").forward(req, resp);
+    }
+
+    /**
+     * 公司注销
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showComCancalPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException  {
+        req.setAttribute(Constants.CURRENT_PAGE, "com_cancal");
+        req.getRequestDispatcher("/WEB-INF/views/service/company_canceled.jsp").forward(req, resp);
+    }
+
+    /**
+     * 食品流通许可证
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showLicensePage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute(Constants.CURRENT_PAGE, "license");
+        req.getRequestDispatcher("/WEB-INF/views/service/food_distribution_license.jsp").forward(req, resp);
+    }
+
+    /**
+     *  餐饮服务许可证
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showServicePermitPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException  {
+        req.setAttribute(Constants.CURRENT_PAGE, "service_permit");
+        req.getRequestDispatcher("/WEB-INF/views/service/food_service_permit.jsp").forward(req, resp);
+    }
+
+    /**
+     * 进口权认定
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showRecognitionPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute(Constants.CURRENT_PAGE, "recognition");
+        req.getRequestDispatcher("/WEB-INF/views/service/import_rights_recognition.jsp").forward(req, resp);
+    }
+
+    /**
+     * 一般纳税人认定
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showIdentifiedPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException  {
+        req.setAttribute(Constants.CURRENT_PAGE, "identified");
+        req.getRequestDispatcher("/WEB-INF/views/service/taxpayer_identified.jsp").forward(req, resp);
+    }
+
+    /**
+     * 商标注册
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void showTrademarkRegPage(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute(Constants.CURRENT_PAGE, "trademark_reg");
+        req.getRequestDispatcher("/WEB-INF/views/service/trademark_reg.jsp").forward(req, resp);
+    }
+
 }
--- web/WEB-INF/views/master/left_sidebar.jsp
+++ web/WEB-INF/views/master/left_sidebar.jsp
@@ -2,16 +2,16 @@
 <div class="col-lg-3 col-md-3 col-sm-4 hidden-xs">
     <div class="list-group">
         <a href="javascript:void(0);" class="list-group-item" style="font-size:16px;font-weight:bold;">服务项目</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "service"}'>active</c:if>">公司注册</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "#"}'>active</c:if>">代理记账</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "#"}'>active</c:if>">乱账整理</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "#"}'>active</c:if>">公司注销</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "#"}'>active</c:if>">进口权认定</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "#"}'>active</c:if>">企业变更</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "#"}'>active</c:if>">商标注册</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "#"}'>active</c:if>">食品流通许可证</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "#"}'>active</c:if>">一般纳税人认定</a>
-        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "#"}'>active</c:if>">餐饮服务许可证</a>
+        <a href="<%=path %>/service/comreg" class="list-group-item <c:if test='${requestScope.page == "comreg"}'>active</c:if>">公司注册</a>
+        <a href="<%=path %>/service/bookkeep" class="list-group-item <c:if test='${requestScope.page == "bookkeep"}'>active</c:if>">代理记账</a>
+        <a href="<%=path %>/service/sort_out" class="list-group-item <c:if test='${requestScope.page == "sort_out"}'>active</c:if>">乱账整理</a>
+        <a href="<%=path %>/service/com_cancal" class="list-group-item <c:if test='${requestScope.page == "com_cancal"}'>active</c:if>">公司注销</a>
+        <a href="<%=path %>/service/recognition" class="list-group-item <c:if test='${requestScope.page == "recognition"}'>active</c:if>">进口权认定</a>
+        <a href="<%=path %>/service/com_change" class="list-group-item <c:if test='${requestScope.page == "com_change"}'>active</c:if>">企业变更</a>
+        <a href="<%=path %>/service/trademark_reg" class="list-group-item <c:if test='${requestScope.page == "trademark_reg"}'>active</c:if>">商标注册</a>
+        <a href="<%=path %>/service/license" class="list-group-item <c:if test='${requestScope.page == "license"}'>active</c:if>">食品流通许可证</a>
+        <a href="<%=path %>/service/identified" class="list-group-item <c:if test='${requestScope.page == "identified"}'>active</c:if>">一般纳税人认定</a>
+        <a href="<%=path %>/service/trademark_reg" class="list-group-item <c:if test='${requestScope.page == "trademark_reg"}'>active</c:if>">餐饮服务许可证</a>
     </div>
 </div>
 
